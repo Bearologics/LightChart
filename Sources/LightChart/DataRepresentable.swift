a7e724e9ec3cdcaa2d0840b95780e66b870dbf1e
Marcus Kida <marcus.kida@bearologics.com>
2023-02-07 08:14:00 +0100

    Always treat chart frame the same, independent of value equality

--- Sources/LightChart/DataRepresentable.swift
+++ Sources/LightChart/DataRepresentable.swift
@@ -21,10 +21,9 @@ extension DataRepresentable {
             vector = Math.stretchIn(vector, offset: offset)
         }
         var points: [CGPoint] = []
-        let isSame = sameValues(in: vector)
         for i in 0..<vector.count {
             let x = frame.size.width / CGFloat(vector.count - 1) * CGFloat(i)
-            let y = isSame ? frame.size.height / 2 : (frame.size.height - lineWidth) * CGFloat(vector[i]) + lineWidth / 2
+            let y = (frame.size.height - lineWidth) * CGFloat(vector[i]) + lineWidth / 2
             points.append(CGPoint(x: x, y: y))
         }
         return points
@@ -39,17 +38,5 @@ extension DataRepresentable {
             case .customFilled(_, let lineWidth, _):
                 return lineWidth
         }
-    }
-    
-    private func sameValues(in vector: [Double]) -> Bool {
-        guard let prev = vector.first else {
-            return true
-        }
-        for value in vector {
-            if value != prev {
-                return false
-            }
-        }
-        return true
     }
 }
